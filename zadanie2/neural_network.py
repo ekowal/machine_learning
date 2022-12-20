6b2dc3548c9524e70ecc23cc6ca40eb0f393c872
emilia_kowal <emilia.kowal.66@gmail.com>
2022-12-20 23:26:52 +0100

    Allow different activation func for every layer

--- zadanie2/neural_network.py
+++ zadanie2/neural_network.py
@@ -6,14 +6,18 @@ import numpy as np
 
 class NeuralNetwork(object):
 
-    def __init__(self, layers_sizes):
+    def __init__(self, layers_sizes: list, act_func_names="sigmoid"):
         self.layers = len(layers_sizes)
-        self.biases = [np.random.randn(y, 1) for y in layers_sizes[1:]]
-        self.weights = [np.random.randn(y, x)
-                        for x, y in zip(layers_sizes[:-1], layers_sizes[1:])]
+        self.biases = [np.random.randn(n_l, 1) for n_l in layers_sizes[1:]]
+        self.weights = [np.random.randn(n_l, x) for x, n_l in zip(layers_sizes[:-1], layers_sizes[1:])]
+
+        if isinstance(act_func_names, str):
+            self.act_func_names = len(layers_sizes) * [act_func_names]
+        else:
+            self.act_func_names = act_func_names
 
 
-    def fit(self, training_data: zip, validation_data: zip, epochs: int=20, batch_size: int=10, c=3.0, verbose=False):
+    def fit(self, training_data: zip, validation_data: zip, epochs: int=20, batch_size: int=10, c: float=3.0, verbose: bool=False):
         training_data = list(training_data)
         validation_data = list(validation_data)
 
@@ -28,7 +32,6 @@ class NeuralNetwork(object):
                 self.batch_fit(batch, c)
             end = time.time()
             if verbose:
-                #accuracy_tr = self.accuracy(training_data, n)
                 accuracy_val = self.accuracy(validation_data, n_val)
                 print(f"Epoch {epoch}:: Validation Accuracy: {accuracy_val}, Time: {end-start}")
 
@@ -39,9 +42,9 @@ class NeuralNetwork(object):
         return(preds)
 
 
-    def feedforward(self, a):
-        for b, w in zip(self.biases, self.weights):
-            a = self.sigmoid(np.dot(w, a)+b)
+    def feedforward(self, a: np.array):
+        for b, w, f_name in zip(self.biases, self.weights, self.act_func_names):
+            a = self.activation(f_name, np.dot(w, a)+b)
         return a
 
 
@@ -50,7 +53,7 @@ class NeuralNetwork(object):
         return sum(int(x == y) for (x, y) in test_results) / n_val
 
 
-    def batch_fit(self, batch, c):
+    def batch_fit(self, batch: np.array, c: float):
         grad_w = [np.zeros(w.shape) for w in self.weights]
         grad_b = [np.zeros(b.shape) for b in self.biases]
         for x, y in batch:
@@ -62,7 +65,8 @@ class NeuralNetwork(object):
         self.biases = [b-(c/len(batch))*nb
                        for b, nb in zip(self.biases, grad_b)]
 
-    def backpropagation(self, x, y):
+
+    def backpropagation(self, x: np.array, y: np.array):
         grad_w = [np.zeros(w.shape) for w in self.weights]
         grad_b = [np.zeros(b.shape) for b in self.biases]
 
@@ -70,20 +74,20 @@ class NeuralNetwork(object):
         activation = x
         activations = [x] # list with all activations
         zs = [] # list to store all the z vectors for the back process
-        for b, w in zip(self.biases, self.weights):
+        for b, w, f_name in zip(self.biases, self.weights, self.act_func_names):
             z = np.dot(w, activation)+b
             zs.append(z)
-            activation = self.sigmoid(z)
+            activation = self.activation(f_name, z)
             activations.append(activation)
 
         # backpropagation
-        delta = self.d_cost(activations[-1], y) * self.d_sigmoid(zs[-1])
+        delta = self.d_cost(activations[-1], y) * self.d_activation(f_name, zs[-1])
         grad_b[-1] = delta
         grad_w[-1] = np.dot(delta, activations[-2].transpose())
 
         for l in range(2, self.layers):
             z = zs[-l]
-            ds = self.d_sigmoid(z)
+            ds = self.d_activation(self.act_func_names[self.layers - l ], z)
             delta = np.dot(self.weights[-l+1].transpose(), delta) * ds
             grad_b[-l] = delta
             grad_w[-l] = np.dot(delta, activations[-l-1].transpose())
@@ -91,14 +95,29 @@ class NeuralNetwork(object):
 
 
     @staticmethod
-    def d_cost(output_activations, y):
+    def d_cost(output_activations: np.array, y: np.array):
         return 2*(output_activations-y)
 
 
     @staticmethod
-    def sigmoid(z):
-        return 1.0/(1.0+np.exp(-z))
+    def activation(function_name: str, x: float):
+        match function_name:
+            case "sigmoid":
+                return 1.0/(1.0+np.exp(-x))
+            case "relu":
+                relu_func = np.vectorize(lambda x: max(0, x))
+                return relu_func(x)
+            case "tanh":
+                return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
 
-    def d_sigmoid(self, z):
-        return self.sigmoid(z)*(1-self.sigmoid(z))
+    def d_activation(self, function_name: str, x: float):
+        match function_name:
+            case "sigmoid":
+                y = self.activation("sigmoid", x)
+                return y*(1-y)
+            case "relu":
+                d_relu_func = np.vectorize(lambda x: 1 if x>0 else 0)
+                return d_relu_func(x)
+            case "tanh":
+                return 1 - self.activation("tanh", x)**2
